20e2d61f58884cb1f6b03d4efee6a509710c5886
hjanday <jandaypreet@gmail.com>
2022-05-15 21:40:45 -0400

    Created initial navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue '
+</script>
+
+<template>
+   <div class="flex justify-left space-x-4 font-thin border-2 border-solid border-black top-0 left-0 right-0 rounded bg-zinc-400 text-white">
+       <a class="mt-2 mb-2 font-semibold ml-4 text-base ">Title</a>
+       <a class ="mt-2 mb-2 text-base">Link 1 </a> 
+       <a class ="mt-2 mb-2 text-base">Link 2 </a>
+       <a class ="mt-2 mb-2 text-base">Link 3 </a>
+   </div>
+
+</template>
