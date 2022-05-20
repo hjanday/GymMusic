5cb2947137873b3848687cbb88e834d5224adb20
hjanday <jandaypreet@gmail.com>
2022-05-19 23:30:47 -0400

    Centered image for exexample and started backend work on api

--- src/components/BackgroundUI.vue
+++ src/components/BackgroundUI.vue
@@ -4,10 +4,11 @@ import { ref } from 'vue '
 
 <template>
 
-<body class="min-h-screen text-center font-semibold">   
-    <p class="ml-6 mt-6">Current Workout: {WORKOUT}</p>
-    <p class="ml-6 mt-2">Current TRACK: {TRACK}</p>
-    <img class="object-center h-36 w-36 justify-center" src = "https://i.scdn.co/image/ab67616d0000b27365d8e34ca619cf5bdc1cbd31">
+<body class="min-h-screen font-semibold">   
+    <p class="ml-6 mt-6 text-center">Current Workout: {WORKOUT}</p>
+    <p class="ml-6 mt-2 text-center">Current TRACK: {TRACK}</p> 
+    <img class = "object-center h-36 w-36 mx-auto flex-auto mt-2" src = "https://i.scdn.co/image/ab67616d0000b27365d8e34ca619cf5bdc1cbd31">
+
 </body>
 
     
