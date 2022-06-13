3c04eb7a117bffbeb0a03fdbb143891b5aee3a26
hjanday <jandaypreet@gmail.com>
2022-06-12 22:07:04 -0400

    Start to write backend

---
+++ src/server/backendserver.js
@@ -0,0 +1,7 @@
+const axios = require('axios')
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+    res.send("Test!")
+})
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
